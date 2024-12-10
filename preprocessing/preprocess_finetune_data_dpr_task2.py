fed1305daf1fe914a33ae475a345807e47e39138
tobiasfink-ai <tobiasfink.ai@gmail.com>
2024-12-10 18:14:30 +0000

    running model training with coliee t2

--- preprocessing/preprocess_finetune_data_dpr_task2.py
+++ preprocessing/preprocess_finetune_data_dpr_task2.py
@@ -10,85 +10,60 @@ from preprocessing.stat_corpus import count_words
 import hydra
 from omegaconf import DictConfig
 random.seed(42)
+from tqdm import tqdm
+from pathlib import Path
 
 
-def read_in_samples_task2(train_dir, labels):
-
-    list_dir = [x for x in os.walk(train_dir)]
+def read_in_samples_task2(case_corpus, labels, mode):
     samples = []
     
-    # TODO: fix after split
-    for sub_dir in list_dir[0][1]:
-
-        # read in query text
-        with open(os.path.join(train_dir, sub_dir, 'entailed_fragment.txt'), 'r') as entailed_fragment:
-            query_text_lines = entailed_fragment.read().splitlines()
-            query_text = ' '.join([text.strip().replace('\n', '') for text in query_text_lines])
-
-        # read in all paragraphs with their names and then choose the relevant ones and sample irrelevant ones!
-        list_sub_dir_paragraphs = [x for x in os.walk(os.path.join(train_dir, sub_dir, 'paragraphs'))]
-        paragraphs_text = {}
-        for paragraph in list_sub_dir_paragraphs[0][2]:
-            with open(os.path.join(train_dir, sub_dir, 'paragraphs', paragraph), 'r') as paragraph_file:
-                para_text = paragraph_file.read().splitlines()[1:]
-                paragraphs_text.update({paragraph: ' '.join([text.strip().replace('\n', '') for text in para_text])})
-
+    for case_id, rel_ids in tqdm(labels.items(), desc=f"Mode: {mode}"):
         positive_ctxs = []
         positive_ids = set()
-        for rel_id in labels[sub_dir]:
+        case_data = case_corpus[case_id]
+        for rel_id in rel_ids:
             positive_ids.add(rel_id)
-            doc_rel_text = paragraphs_text.get(rel_id)
-            ctx = {"title": "",
-                   "text": doc_rel_text,
-                   #"score": 0,
-                   "psg_id": '{}_{}'.format(sub_dir,rel_id.split('.')[0])}
+            doc_rel_text = case_data["paragraph_texts"][rel_id]
+            ctx = {
+                "title": "",
+                "text": doc_rel_text,
+                #"score": 0,
+                "case_id": case_id,
+                "passage_num": Path(rel_id).stem,
+                "psg_id": f"{case_id}_{Path(rel_id).stem}"
+            }
             positive_ctxs.append(ctx)
 
-
-        # read in bm25 scores for hard negatives
-        # with open(os.path.join(train_dir, sub_dir, 'bm25_top200.txt'), "r", encoding="utf8") as out_file:
-        #     top200 = out_file.read().splitlines()
-        # top200_dict = {}
-        # for top in top200:
-        #     top200_dict.update({top.split(' ')[0].strip(): float(top.split(' ')[-1].strip())})
-
-        # hard_negative_ctxs = []
-        # for irrel_id in paragraphs_text.keys():
-        #     if irrel_id not in labels.get(sub_dir):
-        #         doc_rel_text = paragraphs_text.get(irrel_id)
-        #         ctx = {"title": "",
-        #                "text": doc_rel_text,
-        #                "score": top200_dict.get('{}_{}'.format(sub_dir,irrel_id.split('.')[0]))
-        #                if top200_dict.get('{}_{}'.format(sub_dir,irrel_id.split('.')[0])) else 0,
-        #                "psg_id": '{}_{}'.format(sub_dir, irrel_id.split('.')[0])}
-        #         hard_negative_ctxs.append(ctx)
-
-        # # sort list of hard-negatives by score? yes do it!
-        # hard_negative_ctxs.sort(key=lambda hard_negative_ctxs: hard_negative_ctxs.get('score'), reverse=True)
-
-
-        # random negatives
-        # get the same number of negatives as there are positives
-        negative_candidate_ids = list(p_id for p_id in paragraphs_text.keys() if p_id not in positive_ids)
-        random_negative_ids = random.sample(negative_candidate_ids, k=len(positive_ids))
+        negative_candidate_ids = list(p_id for p_id in case_data["paragraph_texts"].keys() if p_id not in positive_ids)
+        negative_sample_k = len(positive_ids)
+        if negative_sample_k > len(negative_candidate_ids):
+            print(f"Small negative sample in case {case_id} with rel_ids {rel_ids}")
+            negative_sample_k = len(negative_candidate_ids)
+        random_negative_ids = random.sample(negative_candidate_ids, k=negative_sample_k)
 
         hard_negative_ctxs = []
         for irrel_id in random_negative_ids:
-            doc_rel_text = paragraphs_text[irrel_id]
-            ctx = {"title": "",
-                    "text": doc_rel_text,
-                    "score": 0,
-                    "psg_id": f"{sub_dir}_{irrel_id.split('.')[0]}"}
+            doc_rel_text = case_data["paragraph_texts"][irrel_id]
+            ctx = {
+                "title": "",
+                "text": doc_rel_text,
+                #"score": 0,
+                "case_id": case_id,
+                "passage_num": Path(irrel_id).stem,
+                "psg_id": f"{case_id}_{Path(irrel_id).stem}"
+            }
             hard_negative_ctxs.append(ctx)
-
-        sample = {"question": query_text,
-                  "answers": [],
-                  "positive_ctxs": positive_ctxs,
-                  "negative_ctxs": [],
-                  "hard_negative_ctxs": hard_negative_ctxs
-                  }
+        
+        sample = {
+            "question": case_data["query_text"],
+            "answers": [],
+            "positive_ctxs": positive_ctxs,
+            "negative_ctxs": [],
+            "hard_negative_ctxs": hard_negative_ctxs
+        }
 
         samples.append(sample)
+
     return samples
 
 
@@ -161,10 +136,15 @@ def write_to_json(samples: list, output_dir: str):
 
 
 def get_label_split(cfg):
-
+    """
+    Since COLIEE 2024 has not provided test set labels, split here from official training set
+    """
     label_file = cfg.task2.label_file_train_original
     label_train_val_split = cfg.task2.label_train_val_split
-    label_train_test_split= cfg.task2.label_train_test_split
+    if cfg.task2.label_train_test_split:
+        label_train_test_split = cfg.task2.label_train_test_split
+    else:
+        label_train_test_split = 0
 
     # first preprocess label file
     with open(label_file, "r") as fp:
@@ -183,13 +163,45 @@ def get_label_split(cfg):
     for idx, label in enumerate(label_keys_list):
         if idx < (total_labels-label_train_val_split-label_train_test_split):
             labels_train[label] = label_dict[label]
-        elif idx < (total_labels-label_train_val_split):
+        elif idx < (total_labels-label_train_test_split):
             labels_val[label] = label_dict[label]
         else:
             labels_test[label] = label_dict[label]
     
 
     return labels_train, labels_val, labels_test
+
+
+def read_corpus(corpus_dir):
+    """
+    Read in corpus from COLIEE task 2. Each case has a query text and paragraph texts of the cited case
+    """
+    task2_cases = os.listdir(corpus_dir)
+    case_corpus = {}
+    for case_id in tqdm(task2_cases, desc="Read Corpus"):
+        # read in query text
+        case_folder = os.path.join(corpus_dir, case_id)
+        if not os.path.isdir(case_folder):
+            continue
+        with open(os.path.join(corpus_dir, case_id, 'entailed_fragment.txt'), 'r') as entailed_fragment:
+            query_text_lines = [line.strip().replace('\n', '') for line in entailed_fragment]
+            query_text = ' '.join(query_text_lines).strip()
+        
+        # read in all paragraphs with their names and then choose the relevant ones and sample irrelevant ones!
+        list_sub_dir_paragraphs = os.listdir(os.path.join(case_folder, 'paragraphs'))
+        paragraph_texts = {}
+        for paragraph in list_sub_dir_paragraphs:
+            paragraph_num = Path(paragraph).stem
+            with open(os.path.join(corpus_dir, case_id, 'paragraphs', paragraph), 'r') as paragraph_file:
+                para_lines = [line.strip().replace('\n', '') for line in paragraph_file][1:] # skip first line, which is just the number
+                para_text = ' '.join(para_lines).strip()
+                paragraph_texts[paragraph_num] = para_text
+        
+        case_corpus[case_id] = {
+            "query_text": query_text,
+            "paragraph_texts": paragraph_texts
+        }
+    return case_corpus
 
 
 @hydra.main(version_base=None, config_path="../config", config_name=None)
@@ -199,36 +211,44 @@ def main(cfg: DictConfig):
 
     datasets = {
         "train":{},
-        "val":{},
-        "test":{}
+        "val":{}
     }
+    if cfg.task2.label_train_test_split:
+        datasets["test"] = {}
 
     for key in datasets.keys():
         datasets[key]["output_dir"] = os.path.join(output_dir, key)
+    
 
     labels_train, labels_val, labels_test = get_label_split(cfg)
 
     label_file_train = cfg.task2.label_file_train
     label_file_val = cfg.task2.label_file_val
-    label_file_test = cfg.task2.label_file_test
 
     datasets["train"]["labels"] = labels_train
     datasets["val"]["labels"] = labels_val
-    datasets["test"]["labels"] = labels_test
 
     with open(label_file_train, "w") as fp:
         json.dump(labels_train, fp, indent=4)
     with open(label_file_val, "w") as fp:
         json.dump(labels_val, fp, indent=4)
-    with open(label_file_test, "w") as fp:
-        json.dump(labels_test, fp, indent=4)
+
+    if cfg.task2.label_train_test_split:
+        datasets["test"] = {}
+        label_file_test = cfg.task2.label_file_test
+        datasets["test"]["labels"] = labels_test
+        with open(label_file_test, "w") as fp:
+            json.dump(labels_test, fp, indent=4)
+
+    case_corpus = read_corpus(corpus_dir)
+    
 
     #jsonl_per_paragraph(test_dir2)
     #search_indices_per_paragraph(train_dir)
 
     for mode, dataset in datasets.items():
         qrels = dataset["labels"]
-        samples = read_in_samples_task2(corpus_dir, qrels)
+        samples = read_in_samples_task2(case_corpus, qrels, mode=mode)
         analyze_samples(samples, dataset["output_dir"])
         write_to_json(samples, dataset["output_dir"])
 
